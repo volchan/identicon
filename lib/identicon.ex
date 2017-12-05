93783b617398aaa81f5a15f1d3aed1f839453db2
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-12-05 18:41:35 +0100

    session 7/49

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,68 @@ defmodule Identicon do
   def main(input) do
     input
       |> hash_input
+      |> pick_color
+      |> build_grid
+      |> filter_odd_squares
+      |> build_pixel_map
+      |> draw_image
+      |> save_image(input)
+  end
+
+  defp save_image(image, filename) do
+    File.write("#{filename}.png", image)
+  end
+
+  defp draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  defp build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  defp filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      code |> rem(2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  defp build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = hex
+            |> Enum.chunk(3)
+            |> Enum.map(&mirror_row/1)
+            |> List.flatten
+            |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  defp mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  defp pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   defp hash_input(input) do
